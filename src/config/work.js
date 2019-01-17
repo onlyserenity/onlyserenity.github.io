e26f3433e920f9dbabd7d3d7759385097473e3b7
Jacob William <amjedcha@gmail.com>
2019-01-16 20:10:51 -0500

    added algo project

--- src/config/work.js
+++ src/config/work.js
@@ -2,12 +2,12 @@ import Reddit from "../assets/reddit.png";
 import Cell from "../assets/cell.png";
 import Flutter from "../assets/flutter.png";
 import Auth from "../assets/authenticator.jpg";
+import Algo from "../assets/algo.jpg";
 
 const Projects = [
   {
     title: "Reddit Client",
     name: "Reddictron",
-    moto: "Fast-performance Reddit client",
     desc:
       "This application is cross-platform and is currently in the alpha stage. New features are added on a daily-basis.",
     languages: ["JavaScript", "ReactJs", "ElectronJs"],
@@ -17,7 +17,6 @@ const Projects = [
   {
     title: "Manufacturing Multi-purpose Application",
     name: "Cell Productivity",
-    moto: "Built with details and perfection",
     desc:
       "This application is currently under development and it's 99% completed. It features real-time analytics, order management and tracking features. It also has in-app messaging system for supervisors and works at workstations and integrated with Workday API to show real-time employee location through out the facility.",
     languages: ["C#", "MVC", "Microsoft SQL", "API"],
@@ -27,7 +26,6 @@ const Projects = [
   {
     title: "Cross-platform Weather App",
     name: "Flutter Weather",
-    moto: "One code, multiple operation systems",
     desc:
       "Weather app developed using Google's newest framework, Flutter. It is designed to target Android and iOS. It uses a free API to fetch weather data and display it to the user.",
     languages: ["Android", "iOS", "Dart", "Flutter", "API"],
@@ -37,11 +35,19 @@ const Projects = [
   {
     title: "Backend Authenticator for Node.js",
     name: "Authenticator.js",
-    moto: "One code, multiple operation systems",
     desc:
       "Backend authentication api that works with nodejs and expressjs. It encrypts relevant information into MongoDB and can retrieve and compare encrypted information such as login for verification.",
     languages: ["ES6", "Node.js", "Passport.js", "JWT"],
     img: Auth,
+    url: ""
+  },
+  {
+    title: "Data Structures and Algorithms in C++",
+    name: "C++ Algorithms",
+    desc:
+      "Implementation of different data structures and algorithms in real world project without the use of any external library.",
+    languages: ["C++", "Data Structures", "Algorithms"],
+    img: Algo,
     url: ""
   }
 ];
